5483a9635bd6b30a3c4efaa91f22c2c31fbe5b83
mehmetulus <43668582+mehmetulus@users.noreply.github.com>
2020-01-17 17:54:55 +0300

    Add files via upload

---
+++ kullanim.php
@@ -0,0 +1,118 @@
+<?php
+public function upload($nesnename,$yuklenecekyer='images/',$tur='img',$w='',$h='',$resimyazisi='')
+	{
+		if($tur=="img")
+		{
+			if(!empty($_FILES[$nesnename]["name"]))
+			{
+				$dosyanizinadi=$_FILES[$nesnename]["name"];
+				$tmp_name=$_FILES[$nesnename]["tmp_name"];
+				$uzanti=$this->uzanti($dosyanizinadi);
+				if($uzanti=="png" || $uzanti=="jpg" || $uzanti=="jpeg" || $uzanti=="gif")
+				{
+					$classIMG=new upload($_FILES[$nesnename]);
+					if($classIMG->uploaded)
+					{
+						if(!empty($w))
+						{
+							if(!empty($h))
+							{
+								$classIMG->image_resize=true;
+								$classIMG->image_x=$w;
+								$classIMG->image_y=$h;
+							}
+							else
+							{
+								if($classIMG->image_src_x>$w)
+								{
+									$classIMG->image_resize=true;
+									$classIMG->image_ratio_y=true;
+									$classIMG->image_x=$w;
+								}
+							}
+						}
+						else if(!empty($h))
+						{
+								if($classIMG->image_src_h>$h)
+								{
+									$classIMG->image_resize=true;
+									$classIMG->image_ratio_x=true;
+									$classIMG->image_y=$h;
+								}
+						}
+						
+						if(!empty($resimyazisi))
+						{
+							$classIMG->image_text = $resimyazisi;
+
+						$classIMG->image_text_direction = 'v';
+						
+						$classIMG->image_text_color = '#FFFFFF';
+						
+						$classIMG->image_text_position = 'BL';
+						}
+						$rand=uniqid(true);
+						$classIMG->file_new_name_body=$rand;
+						$classIMG->Process($yuklenecekyer);
+						if($classIMG->processed)
+						{
+							$resimadi=$rand.".".$classIMG->image_src_type;
+							return $resimadi;
+						}
+						else
+						{
+							return false;
+						}
+					}
+					else
+					{
+						return false;
+					}
+				}
+				else
+				{
+					return false;
+				}
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else if($tur=="ds")
+		{
+			
+			if(!empty($_FILES[$nesnename]["name"]))
+			{
+				
+				$dosyanizinadi=$_FILES[$nesnename]["name"];
+				$tmp_name=$_FILES[$nesnename]["tmp_name"];
+				$uzanti=$this->uzanti($dosyanizinadi);
+				if($uzanti=="doc" || $uzanti=="docx" || $uzanti=="pdf" || $uzanti=="xlsx" || $uzanti=="xls" || $uzanti=="ppt" || $uzanti=="xml" || $uzanti=="mp4" || $uzanti=="avi" || $uzanti=="mov")
+				{
+					
+					$classIMG=new upload($_FILES[$nesnename]);
+					if($classIMG->uploaded)
+					{
+						$rand=uniqid(true);
+						$classIMG->file_new_name_body=$rand;
+						$classIMG->Process($yuklenecekyer);
+						if($classIMG->processed)
+						{
+							$dokuman=$rand.".".$uzanti;
+							return $dokuman;
+						}
+						else
+						{
+							return false;
+						}
+					}
+				}
+			}
+		}
+		else
+		{
+			return false;
+		}
+	}
+?>
